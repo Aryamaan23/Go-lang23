5f83d96c36de5711969beaa785d384c878670606
Aryamaan <pandeyaryamaan@gmail.com>
2021-10-17 14:17:35 +0530

    feat: add files

---
+++ golang-17-files/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to files system in Go23!")
+	content := "This needs to go in a file- LearnCodeOnline.in"
+	file, err := os.Create("./mylcogofile.txt")
+	if err != nil {
+		panic(err)
+
+	}
+	length, err := io.WriteString(file, content)
+	if err != nil {
+		panic(err)
+
+	}
+	fmt.Println("Length is: ", length)
+	defer file.Close()
+	readFile("./mylcogofile.txt")
+
+}
+
+func readFile(filename string) {
+	databyte, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+
+	}
+	fmt.Println("Text data inside the file is \n", string(databyte))
+
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+
+	}
+
+}
